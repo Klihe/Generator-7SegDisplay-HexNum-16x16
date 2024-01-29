b2cc795b5adf9e1143c25c4f4466b0b7fc149303
Klihe <stepan.birke@gmail.com>
2024-01-29 18:28:32 +0100

    Init - V0

---
+++ main.py
@@ -0,0 +1,36 @@
+# main.py
+
+# Bit values for 7 segment display
+ZERO = ["e", "7"]
+ONE = ["2", "1"]
+TWO = ["c", "b"]
+THREE = ["6", "b"]
+FOUR = ["2", "d"]
+FIVE = ["6", "e"]
+SIX = ["e", "e"]
+SEVEN = ["2", "3"]
+EIGHT = ["e", "f"]
+NINE = ["6", "f"]
+
+A = ["a", "f"]
+B = ["f", "f"]
+C = ["c", "6"]
+D = ["f", "7"]
+E = ["c", "e"]
+F = ["8", "e"]
+
+# Array of values on display
+NUMBERS = [ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, A, B, C, D, E, F]
+
+# Writing displayed values 0-255 (16x16)
+for i in NUMBERS:
+    for j in NUMBERS:
+        # Writing to the output file
+        with open('output.txt', 'a') as file:
+            file.write(f"{j[0]}{i[0]}{i[1]}{j[1]} ")
+        # Check the values in the terminal
+        print(j[0],i[0],i[1],j[1])
+
+    # Add enter after 16. no
+    with open('output.txt', 'a') as file:
+        file.write("\n")
